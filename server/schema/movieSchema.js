72261ad917b716af45414089242249372c404a13
maria vasquez <maria.vasquez@developersinstitute.co.nz>
2023-05-28 14:35:26 +1200

    Add Schema : Movie & User

---
+++ server/schema/movieSchema.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const movieSchema = new Schema({
+  actors: [String],
+  director: String,
+  duration: String,
+  genres: [String],
+  host: String,
+  image: String,
+  synopsis: String,
+  title: String,
+  type: String,
+  url: String,
+  country: String,
+  reviews: [
+    {
+      id: String,
+      userId: String,
+      rating: Number,
+      content: String,
+      timestamp: String,
+    },
+  ],
+  userCreator: {
+    userName: String,
+    userId: String,
+  },
+  id: String,
+  releaseYear: String,
+  likes: Number,
+  dislikes: Number,
+});
+
+const Movie = mongoose.model("Movie", movieSchema);
+module.exports = Movie;
