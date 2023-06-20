c8bcc37979c8b6c07358976b587d986241a9fbef
maria vasquez <maria.vasquez@developersinstitute.co.nz>
2023-06-21 11:01:27 +1200

    try Searchbox with fetch request

--- my-app/src/components/Search.js
+++ my-app/src/components/Search.js
@@ -1,18 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 // import { useNavigate } from "react-router-dom";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./Search.css";
-import axios from "axios";
+// import axios from "axios";
 
 const Search = ({ handleSearch }) => {
   const [key, setKey] = useState("");
-  const [searchResult, setSearchResult] = useState("");
+  // const [searchResult, setSearchResult] = useState("");
   // const navigate = useNavigate();
 
-  const handleChange = (e) => {
-    setKey(e.target.value);
-  };
+  // const handleChange = (e) => {
+  //   setKey(e.target.value);
+  // };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -22,39 +22,61 @@ const Search = ({ handleSearch }) => {
     // }
   };
 
-  useEffect(() => {
-    const search = async () => {
-      try {
-        if (!key.trim()) {
-          setSearchResult([]);
-          return;
-        }
-        const res = await axios.get("http://localhost:5001/movies", {
-          params: { key: key, limit: 5 },
-        });
-        setSearchResult(res.data.data);
-        console.log(res);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    search();
-  }, [key]);
+  // useEffect(() => {
+  //   const search = async () => {
+  //     try {
+  //       if (!key.trim()) {
+  //         setSearchResult([]);
+  //         return;
+  //       }
+  //       const res = await axios.get("http://localhost:5001/movies", {
+  //         params: { key: key, limit: 5 },
+  //       });
+  //       setSearchResult(res.data.data);
+  //       console.log(res);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
+  //   search();
+  // }, [key]);
+
+  const fetchData = (value) => {
+    fetch("http://localhost:5001/movies")
+      .then((response) => response.json())
+      .then((json) => {
+        // const results = json.filter((movie) => {
+        // return (
+        //     value &&
+        //     movie &&
+        //     movie.title &&
+        //     movie.title.toLowerCase().includes(value)
+        //   );
+        // });
+        console.log(json);
+      });
+  };
+
+  const handleChange = (value) => {
+    setKey(value);
+    fetchData(value);
+  };
 
   return (
     <div>
       <form className="search-flex" onSubmit={handleSubmit}>
+        {/* <button className="btn-icon" type="submit"> */}
+        <FontAwesomeIcon className="icon-search" icon={faSearch} />
+        {/* </button> */}
         <input
           type="text"
-          className="search form-control"
+          className="search"
           placeholder="Search movies..."
           value={key}
           onChange={handleChange}
         />
-        <button className="btn-icon" type="submit">
-          <FontAwesomeIcon className="icon-search" icon={faSearch} />
-        </button>
-        {searchResult && searchResult.length > 0 && (
+
+        {/* {searchResult && searchResult.length > 0 && (
           <div className="search-result">
             {searchResult.map((movie) => (
               <div className="result-item" key={movie.id}>
@@ -67,7 +89,7 @@ const Search = ({ handleSearch }) => {
               </div>
             ))}
           </div>
-        )}
+        )} */}
       </form>
     </div>
   );
